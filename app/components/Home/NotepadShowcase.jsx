3d55625bcd478041f961f13c9df7590d7e2eabe2
Adrian Li <li.adrianmc@gmail.com>
2016-04-14 17:14:13 +0800

    fixed styles for notepad showcase

--- app/components/Home/NotepadShowcase.jsx
+++ app/components/Home/NotepadShowcase.jsx
@@ -18,42 +18,30 @@ export default class NotepadShowcase extends React.Component {
             </Col>
           </Row>
         </Grid>
+        <div className="notepad-details-container">
+          <div className="notepad-img">
+            <img
+              src="/assets/img/main-notepad.png"
+              alt="main app screenshot"
+              className="img-responsive"
+            />
+          </div>
+          <div className="notepad-desc">
+            <div className="notepad-text">
+              <h2>The Notepad is here</h2>
+              <p>Don’t lose track of the important things you need to do in the chaos of chat.</p>
+              <p>The Notepad allows your whole team to write meeting minutes, manage tasks, track packages, analyze spreadsheets, brainstorm, and much more.</p>
+              <p>Collaborate conveniently in an easy to access location.</p>
+            </div>
 
-        <div className="features">
-          <div className="features-section reverse">
-            <div className="features-section-container">
-              <div className="features-img">
-                <img
-                  src="/assets/img/main-notepad.png"
-                  alt="main app screenshot"
-                  className="img-responsive notepad-img notepad-img-desktop"
-                />
-                <img
-                  src="/assets/img/main-notepad.png"
-                  alt="main app screenshot"
-                  className="img-responsive notepad-img notepad-img-mobile"
-                />
-              </div>
-              <div className="features-text">
-                <div className="notepad-text">
-                  <h2>The Notepad is here</h2>
-                  <p>Don’t lose track of the important things you need to do in the chaos of chat.</p>
-                  <p>The Notepad allows your whole team to write meeting minutes, manage tasks, track packages, analyze spreadsheets, brainstorm, and much more.</p>
-                  <p>Collaborate conveniently in an easy to access location.</p>
-                </div>
-
-                <div className="notepad-text">
-                  <h2>Bots are here to help</h2>
-                  <p>Bots are your business’ best friend.</p>
-                  <p>The Notepad isn’t just limited to simple collaborative tools. We at Olis are constantly developing the next generation of enterprise-use smart bots that can help your company efficiently and effectively automate important work so your team can focus on being more productive.</p>
-                  <p>An API will also be available soon for companies to quickly develop their own special bots to handle their own unique problems.</p>              
-                </div>
-              </div>              
+            <div className="notepad-text">
+              <h2>Bots are here to help</h2>
+              <p>Bots are your business’ best friend.</p>
+              <p>The Notepad isn’t just limited to simple collaborative tools. We at Olis are constantly developing the next generation of enterprise-use smart bots that can help your company efficiently and effectively automate important work so your team can focus on being more productive.</p>
+              <p>An API will also be available soon for companies to quickly develop their own special bots to handle their own unique problems.</p>              
             </div>
           </div>
-
         </div>
-
         <div className="white-bar"></div>
       </div>
     );
