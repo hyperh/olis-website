3e07dee48b40fb138e0ae0658efa88e85926fe8f
HL <hi.heyse@gmail.com>
2016-10-19 14:25:58 -0400

    Use easier way to deploy to github pages

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-brunch build --production
-git add public && git commit -m 'new build'
-git subtree split --prefix public -b gh-pages
-git push -f origin gh-pages:gh-pages
-git branch -D gh-pages
